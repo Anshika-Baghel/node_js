af7610fed727fbcffd273c6401d010763334912c
Anshika-Baghel <anshikabaghel2001@gmail.com>
2024-09-04 15:56:53 +0530

    use of app.get

--- create_server.js
+++ create_server.js
@@ -9,4 +9,18 @@ app.get('/chicken',(req,res)=>{
     res.send('sure sir I would love to serve chicken')
 })
 
-app.listen(3000)
+app.get('/idli',(req,res)=>{
+
+    var customized_idli={
+        name:'rava idli',
+        size:'10 cm in diameter',
+        is_sambhar:true,
+        is_chutney:false
+
+    }
+    res.send(customized_idli)
+})
+
+app.listen(3000,()=>{
+        console.log('server listening on port 3000')
+})
